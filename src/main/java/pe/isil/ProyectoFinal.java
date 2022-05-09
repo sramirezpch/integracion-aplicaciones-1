841f9a4defcb8aebfbf0c8cef1f3e6f52c24035a
sergio.ramirez <sramirezpch@gmail.com>
2022-05-09 18:35:25 -0500

    Added ER Diagrams in .mdj (StarUML file format) and PNG

---
+++ src/main/java/pe/isil/ProyectoFinal.java
@@ -0,0 +1,13 @@
+package pe.isil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinal {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoFinal.class, args);
+    }
+
+}
